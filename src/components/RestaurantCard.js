d328f5813b34699c9b0a98615beb21910b7b23be
Shekhu04 <gshikhar04@gmail.com>
2024-08-26 13:41:51 +0530

    Completed

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,6 +3,8 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
 
+  console.log(resData);
+
   // Optional Chaining
   const {
     cloudinaryImageId,
